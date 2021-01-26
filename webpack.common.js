eff547ec307e7db7e53d5489ba6c94f64bc94550
terrainagency <tony@terrain.agency>
2021-01-26 14:27:21 -0600

    Webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,29 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: "./src/app.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/layout/theme.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            // {
+            //     test: /\.(svg|png|jpg|gif)$/,
+            //     use: {
+            //       loader: "file-loader",
+            //       options: {
+            //         name: "[name].[contenthash].[ext]",
+            //         outputPath: "assets"
+            //       }
+            //     }
+            // }
+        ]
+    }
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,29 +1,11 @@
-const path = require("path")
+const common = require("./webpack.common")
+const { merge } = require("webpack-merge")
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
-    output: {
-        filename: "./src/app.js",
-        path: path.resolve(__dirname, "dist"),
-    },
     module: {
         rules: [
-            // {
-            //     test: /\.css$/,
-            //     exclude: /node_modules/,
-            //     use: ['style-loader', 'postcss-loader'],
-            // },
-            {
-                // Output all images into dist/assets
-                test: /\.(svg|png|jpg|gif)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[hash].[ext]",
-                        outputPath: "assets"
-                    }
-                }
-            }
+
         ]
     }
-}
+}) 
